2c5df791db13b08d29fc62cc919c7ba3bff1a0bd
panyushan <1193345664@qq.com>
2022-04-14 23:41:25 +0800

    配置ignore忽略文件以及stylelint相关配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  "extends": "stylelint-config-standard"
+  plugins: ['stylelint-scss'],
+  extends: ["stylelint-config-standard","stylelint-config-standard-scss"],
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+  rules:{
+    // "selector-class-pattern":"^([a-z][a-z0-9]*)(_[a-z0-9]+)*$",  //classname命名方式
+    'block-no-empty': true, // 禁止空块
+    "comment-no-empty": true, // 禁止空注释
+    "no-empty-first-line": true // 禁止空第一行
+   }
 };
