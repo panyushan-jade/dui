011ae4af9dd391e1f20a310ee28a5cf288038faf
panyushan <1193345664@qq.com>
2022-04-24 20:22:01 +0800

    index on main: 8c608b0 feat: button初步相关代码

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,13 @@ function App() {
   return (
     <div className="App">
       <Button type="link">我是link</Button>
+      <Button>我是default</Button>
+      <Button type="primary">我是primary</Button>
+      <Button danger>我是primary danger</Button>
+      <Button size="large">我是large</Button>
+      <Button size="small">我是small</Button>
+      <Button size="small" disabled>我是disbled</Button>
+      <Button type="link" disabled className="pan">a disabled</Button>
     </div>
   );
 }
