095e40a989a853f03581535529013d42960b6269
panyushan <1193345664@qq.com>
2022-08-02 19:36:46 +0800

    feat: transition

--- src/components/Transition/index.tsx
+++ src/components/Transition/index.tsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { CSSTransition } from "react-transition-group";
+import { CSSTransitionProps } from "react-transition-group/CSSTransition";
 
-export interface ITransitionProps {
+export type AnimationName =
+  | "zoom-in-top"
+  | "zoom-in-left"
+  | "zoom-in-bottom"
+  | "zoom-in-right";
+
+export type TransitionProps = CSSTransitionProps & {
+  animation?: AnimationName;
   className?: string;
-}
-
-const Transition: React.FC<ITransitionProps> = (props) => {
-  console.log("props===>", props);
-  return <div></div>;
 };
+// interface TransitionProps extends CSSTransitionClassNames {
+//   animation?: AnimationName
+//   className?: string
+// }
+
+const Transition: React.FC<TransitionProps> = (props) => {
+  const { children, className, animation, ...restProps } = props;
+  return (
+    <CSSTransition
+      classNames={className ? className : animation}
+      {...restProps}
+    >
+      <div>{children}</div>
+    </CSSTransition>
+  );
+};
+
+// Transition.defaultProps = {
+//   unmountOnExit: true,
+//   appear: true,
+// }
 
 export default Transition;
