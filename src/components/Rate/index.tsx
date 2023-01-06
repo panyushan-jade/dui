e86e6c1018d8c67e2e1b71590d6f143b64d409b2
panyushan <1193345664@qq.com>
2023-01-06 18:30:54 +0800

    perf: feat: rate组件初步布局

--- src/components/Rate/index.tsx
+++ src/components/Rate/index.tsx
@@ -1,4 +1,4 @@
-import React from "react"; // , { ReactNode }
+import React, { useState } from "react"; // , { ReactNode }
 import classnames from "classnames";
 import Icon from "../IconFont";
 
@@ -26,20 +26,57 @@ export interface RateProps {
 const Rate: React.FC<RateProps> = (props) => {
   const { className, allowHalf } = props;
   console.log("allowHalf: ", allowHalf);
-  const classNames = classnames("dui-rate", className);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [ids, setIds] = useState<number[]>([0, 1, 2, 3, 4]);
+  const [pos, setPos] = useState<any>({
+    curIndexLi: -1,
+    curIndexStar: -1,
+  });
+  // const [classNames,setClassNames] = useState<Record<string,any>>({
+  //   'dui-rate-star': true, // 基础类
+  //   'dui-rate-star-zero': false, // 0 星
+  //   'dui-rate-star-half': false, // 半星
+  //   'dui-rate-star-full': false, // 满星
+  //   'dui-rate-star-active': false, // 当前激活星
+  // })
+
+  const onMouseFirstEnter = (d: number, curStar: number) => {
+    console.log("onMouseFirstEnter");
+    const curIndex = ids.findIndex((id) => id == d);
+    console.log("curIndex: ", curIndex, curStar);
+    setPos({
+      curIndexLi: curIndex,
+      curIndexStar: curStar,
+    });
+  };
+
+  const renderClassNames = (key: number) => {
+    console.log("key: ", key);
+    let classNames = "dui-rate-star";
+    if (pos.curIndexLi === -1) {
+      classNames += " dui-rate-star-zero";
+    }
+    return classNames;
+  };
 
   return (
-    <ul className={classNames}>
-      {new Array(5).fill("").map(() => {
+    <ul className={classnames("dui-rate", className)}>
+      {ids.map((key) => {
         return (
-          <li className="dui-rate-star" key={crypto.randomUUID()}>
-            <div className="dui-rate-star-first">
+          <li className={renderClassNames(key)} key={key}>
+            <div
+              className="dui-rate-star-first"
+              onMouseEnter={() => onMouseFirstEnter(key, 0)}
+            >
               <Icon
                 type="icon-star"
                 style={{ fontSize: 20, color: "#f0f0f0" }}
               />
             </div>
-            <div className="dui-rate-star-second">
+            <div
+              className="dui-rate-star-second"
+              onMouseEnter={() => onMouseFirstEnter(key, 1)}
+            >
               <Icon
                 type="icon-star"
                 style={{ fontSize: 20, color: "#f0f0f0" }}
